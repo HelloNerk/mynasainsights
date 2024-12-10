989bdfccc58cf36c5d2ab85d98e853078f3d3883
Joseph Huamani | Nerk <112527806+HelloNerk@users.noreply.github.com>
2024-12-10 10:51:45 -0500

    Update app_constants.dart

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants{
   //https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2017-07-08&end_date=2017-12-10
   static const String baseUrl="https://api.nasa.gov/planetary";
   static const String endpoint='/apod?api_key=';
-  static const String apiKey="wDCGrEa1aQHa5tTbs3RdsbkllVsHk5Toniglj51u";
+  static const String apiKey="apikey";
   static const String dateRange="&start_date=2017-07-08&end_date=2017-12-10";
 
   static const String backgroundImage = 'assets/images/espacio.png'; // Ruta de imagen de fondo
